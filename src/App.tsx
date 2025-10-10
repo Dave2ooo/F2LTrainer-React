6e5bd4dc0e610da1ad9d550c79d1b1d6fbcd62b1
Dave2ooo <david_schweighofer@gmx.at>
2025-10-10 23:19:40 +0200

    Card component test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 
 import TwistyPlayer, { type TwistyPlayerHandle } from "./components/TwistyPlayer";
+import { Card } from "@chakra-ui/react";
 
 function App() {
   const twistyRef = useRef<TwistyPlayerHandle>(null);
@@ -8,15 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <TwistyPlayer
-          ref={twistyRef}
-          alg={"R U R' U'"}
-          puzzle="3x3x3"
-          controlPanel="bottom-row"
-          visualization="3D"
-          background="none"
-          style={{ width: 360, height: 360 }}
-        />
+        <Card.Root>
+          <Card.Header>
+            <TwistyPlayer
+              ref={twistyRef}
+              alg={"R U R' U'"}
+              controlPanel="auto"
+              experimentalDragInput="auto"
+              background="none"
+              experimentalStickering="EDGES:------------,CORNERS:--------,CENTERS:-----`;"
+              style={{ width: 250, height: 250 }}
+            />
+          </Card.Header>
+          <Card.Body />
+          <Card.Footer />
+        </Card.Root>
       </div>
     </>
   );
