a486eb49cc0facfc55aca1579fe79c4959b4759e
Dave2ooo <david_schweighofer@gmx.at>
2025-10-11 22:43:08 +0200

    added scrambles

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+export { basicAlgorithms } from "./algorithms/basic_algorithms";
+export { basicAlgorithmsBack } from "./algorithms/basic_algorithms_back";
+export { advancedAlgorithms } from "./algorithms/advanced_algorithms";
+export { expertAlgorithms } from "./algorithms/expert_algorithms";
+
+export { basicScrambles } from "./scrambles/basic_scrambles";
+export { basicScramblesBack } from "./scrambles/basic_scrambles_back";
+export { advancedScrambles } from "./scrambles/advanced_scrambles";
+export { expertScrambles } from "./scrambles/expert_scrambles";
