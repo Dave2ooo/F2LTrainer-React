266131e4aec2e4a1ebcdbb7cf30de19ea2ab55a4
Dave2ooo <david_schweighofer@gmx.at>
2025-10-12 07:27:19 +0200

    added global state skeleton

--- src/types/group.ts
+++ src/types/group.ts
@@ -1,1 +1,26 @@
 export type Group = "basic" | "basicBack" | "advanced" | "expert";
+export type TrainState = "unlearned" | "learning" | "finished";
+
+type CaseId = number; // 1-based, matches existing assets
+
+interface CaseState {
+    status: 0 | 1 | 2; // learning, unlearned, finished / could also be "unlearned" | "learning" | "finished"
+    algorithmSelection: { left: number; right: number };
+    customAlgorithm: { left: string; right: string };
+    identicalAlgorithm: boolean;
+    solveCount: number;
+}
+
+interface GroupState {
+    cases: Record<CaseId, CaseState>;
+    collapsedCategories: Record<number, boolean>;
+}
+
+interface GlobalState {
+    groups: Record<Group, GroupState>;
+    currentGroup: Group; // Group that is selected in selection view
+    trainStateSelection: Record<TrainState, boolean>;
+    trainGroupSelection: Record<Group, boolean>;
+    trainSideSelection: Record<"left" | "right", boolean>;
+    colorSelection: Record<"cross" | "front", string>;
+}
